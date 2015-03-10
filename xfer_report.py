43c0f68250fb763ff8e21e8cd9bf45f7442af6f9
Vince Star <vince.star@gtechassociates.net>
2015-03-10 16:45:09 -0400

    Initial commit, Jackson

---
+++ xfer_report.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys, os, MySQLdb, SocketServer, SimpleHTTPServer, urllib, urlparse
+
+class myHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+        def do_GET(self):
+                #date1 = urlparse.parse_qs(urlparse.urlparse(self.path).query).get('date1', 'err')
+                self.send_response(200)
+                self.send_header("Content-type", "text/plain")
+                self.end_headers()
+		if date1 != 'err':
+			output = do_the_big_query(date1[0])
+			if len(output) < 1:
+				self.wfile.write('NO DIFFS')
+			else:
+				self.wfile.write(output)
+		f.close
+                self.finish()
+
+def do_the_big_query(date1):
+	con = MySQLdb.connect(host="localhost",user="gta_ro",passwd="glacier",db="cm")
+	cur = con.cursor()
+	query = "SELECT switch_id FROM control.switch_id WHERE switch = '" + switch + "' LIMIT 1"
+	output = ""
+	cur.execute(query)
+	rows = cur.fetchall()
+	for row in rows:
+		table_no = row[0]
+		if table_no != prev_table:
+	return output
+
+httpd = SocketServer.ThreadingTCPServer(('', 3010), myHandler)
+httpd.serve_forever()
